dee434807beade2c5d6c7b5994e5625f11a4dccb
Steven Abrams <steven.abrams86@gmail.com>
2015-06-11 14:35:08 -0600

    finished app with web sequence diagram

---
+++ readme.md
@@ -0,0 +1,3 @@
+GitHub URL:  https://github.com/sabrams86/express-air-travel-calculator
+
+![](http://www.websequencediagrams.com/cgi-bin/cdraw?lz=Y2xpZW50LT5pbmRleC5qczogR0VUCgAGCAAPCWFkZToADghhZGUtPmxheW91dAASBwACCwA_CwA7CwBgBjogcmVuZGVyIHdpdGggMjAwIE9LCgBvElBPUwB2DGZ1bmN0aW9uOiBDYWxjdWxhdGUgUHJpY2UKABIIAIEyDHJldHVybgAaBwBsW3QAgTkO&s=qsd)
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var logic = require('./../public/javascripts/logic.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Air Travel Calculator' });
+  res.render('index', { title: 'Air Travel Calculator'});
+});
+
+
+router.post('/', function(req, res, next) {
+  var departure = req.body.departure;
+  var arrival = req.body.arrival;
+  var bags = req.body.bags;
+  var planeClass = req.body.flightclass;
+  var wifi = req.body.wifi;
+  var dcode = req.body.discount;
+  var output = logic.calculatePrice(departure, arrival, bags, planeClass, wifi, dcode)
+  output = output.toFixed(2);
+  res.render('index', { title: 'Air Travel Calculator', total: output });
 });
 
 module.exports = router;
